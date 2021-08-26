e3309898bad51103066c9aa079739c4366ff1096
Kacper <kacperpeper@gmail.com>
2021-08-26 15:53:34 +0200

    login view without hover states is done

--- Tasks/urls.py
+++ Tasks/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('register', views.register, name='register'),
-    path('register', views.login, name= 'login')
+    path('login', views.login, name= 'login')
 ]
