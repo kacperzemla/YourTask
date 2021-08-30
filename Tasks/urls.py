de0cb4b48771e2d304a2d022a97e002d50a189fa
Kacper <kacperpeper@gmail.com>
2021-08-30 12:56:33 +0200

    all links working

--- Tasks/urls.py
+++ Tasks/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='main'),
     path('register', views.register, name='register'),
     path('login', views.login, name='login')
 ]
