7f01fed7a5f5c485cb25071e81718e2052a94c4f
Kacper <kacperpeper@gmail.com>
2021-09-11 23:58:58 +0200

    made goal model and customer model

--- Tasks/models.py
+++ Tasks/models.py
@@ -1,2 +1,20 @@
 from django import forms
 from django.db import models
+from django.contrib.auth.models import User
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete = models.CASCADE)
+    name = models.CharField(max_length=200 ,null = True)
+    phone = models.CharField(max_length=200, null = True)
+    # email = models.CharField(max_length=200, null = True)
+    date_created = models.DateTimeField(auto_now_add = True, null = True)
+
+    def __str__(self):
+        return self.name
+
+class Todo(models.Model):
+    text = models.CharField(max_length=40)
+    complete = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.text
