6e0daab25a48590a3cda22ef287c8d9104a0493f
Kacper <kacperpeper@gmail.com>
2021-09-12 23:24:00 +0200

    added goals form and rendered goals

--- Tasks/forms.py
+++ Tasks/forms.py
@@ -1,10 +1,20 @@
+from Tasks.models import Goal
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from django.contrib.auth.models import User
+from .models import Goal
 # Create your models here.
 
 class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'password1', 'password2']
+
+class GoalForm(ModelForm):
+    class Meta:
+        model = Goal
+        fields = ['text']
+
+    def __init__(self, *args, **kwargs):
+        super(GoalForm, self).__init__(*args, **kwargs)
